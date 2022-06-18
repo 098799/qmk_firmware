b474a4e9102b02da6e8baa6850ffab92dd7ee534
Tomasz Grining <tomek.grining@gmail.com>
2022-06-18 13:42:04 +0200

    Initial implementation of the keymap for crkbd

---
+++ keyboards/crkbd/keymaps/tg/rules.mk
@@ -0,0 +1,18 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+
+TAP_DANCE_ENABLE = yes
+SRC += features/caps_word.c
+
+# Trying to save space on the controller
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+EXTRAKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+
+LTO_ENABLE      = yes
