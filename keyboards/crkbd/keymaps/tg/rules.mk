ec0ff3e46909f15ff90f01f275344baa3a5cfc37
Tomasz Grining <tomek.grining@gmail.com>
2022-06-24 12:41:50 +0200

    Slowly evolving the crkbd

--- keyboards/crkbd/keymaps/tg/rules.mk
+++ keyboards/crkbd/keymaps/tg/rules.mk
@@ -1,10 +1,10 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
+MOUSEKEY_ENABLE = yes
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 
-TAP_DANCE_ENABLE = yes
-SRC += features/caps_word.c
+TAP_DANCE_ENABLE = no
+# SRC += features/caps_word.c
 
 # Trying to save space on the controller
 CONSOLE_ENABLE = no
@@ -15,4 +15,4 @@ GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
 MUSIC_ENABLE = no
 
-LTO_ENABLE      = yes
+LTO_ENABLE = yes
